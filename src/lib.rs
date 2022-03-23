c78c3b9371754239f1622486726d8316419f3455
Martin Wang <wangxuesong@gmail.com>
2022-03-23 22:04:24 +0800

    parse server sample

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
 use anyhow::Result;
 use goblin::elf::Elf;
-use id_tree::{Node, Tree};
-
+use id_tree::InsertBehavior::AsRoot;
+use id_tree::{Node, Tree, TreeBuilder};
 
 type DependencyTree = Tree<BinaryFile>;
 type DependencyNode = Node<BinaryFile>;
 
 #[derive(Debug)]
 pub struct BinaryFile {
-    pub name: String,
+    pub path: String,
     pub is_root: bool,
     pub is_executable: bool,
 }
 
 impl BinaryFile {
-    pub fn new(name: String) -> Result<BinaryFile> {
-        let data = std::fs::read(name.clone())?;
+    pub fn new(path: String) -> Result<BinaryFile> {
+        let data = std::fs::read(path.clone())?;
         let elf = Elf::parse(&data)?;
         let mut is_executable = false;
         for header in elf.program_headers.iter() {
@@ -31,7 +31,7 @@ impl BinaryFile {
         }
 
         let binary_file = BinaryFile {
-            name,
+            path,
             is_root: false,
             is_executable,
         };
@@ -43,11 +43,30 @@ impl BinaryFile {
     }
 }
 
+pub fn parse_binary_file(path: &str) -> Result<DependencyTree> {
+    let mut tree = TreeBuilder::new().with_node_capacity(5).build();
+    let root = BinaryFile {
+        path: path.to_string(),
+        is_root: true,
+        is_executable: false,
+    };
+    tree.insert(DependencyNode::new(root), AsRoot)?;
+    Ok(tree)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::path::Path;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn parse_server() {
+        let data_path = Path::new(std::env!("CARGO_MANIFEST_DIR")).join("tests/elfbin");
+        let server_path = data_path.join("bin/server");
+        let tree = parse_binary_file(server_path.to_str().unwrap()).unwrap();
+        let root_id = tree.root_node_id().unwrap();
+        let root = tree.get(root_id).unwrap().data();
+        assert_eq!(root.path, server_path.to_str().unwrap().to_string());
+        assert_eq!(tree.height(), 1);
     }
 }
